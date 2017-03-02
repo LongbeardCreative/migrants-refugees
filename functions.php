af23721c8760475f460038131e6230eb6b749dfa
Evan Hennessy <hello@hennessyevan.com>
2017-03-01 19:23:25 -0500

    Nightly Merge

--- functions.php
+++ functions.php
@@ -10,9 +10,15 @@ add_action( 'wp_enqueue_scripts', 'wp_schools_enqueue_scripts', 11);
 
 define('WP_SCSS_ALWAYS_RECOMPILE', true);
 
+// enqueue the child theme customJS script
+function lb_enqueue_scripts() {
+wp_register_script('childjs', get_stylesheet_directory_uri() . '/js/lb_custom.js', array('jquery'), true);
+wp_enqueue_script('childjs');
+}
+add_action( 'wp_enqueue_scripts', 'lb_enqueue_scripts' );
+
+// add lb_ classes to all applicable VC elements
 add_action( 'vc_after_init', 'add_lb_custom_class' );/* Note: here we are using vc_after_init because WPBMap::GetParam and mutateParame are available only when default content elements are "mapped" into the system */
-
-
 function add_lb_custom_class() {
     //Get array of vc elements minus 'vc_' that support custom class "el_class"
     $elements = array(
@@ -23,7 +29,7 @@ function add_lb_custom_class() {
         'vc_row_inner'      => 'lb_row_inner',
         'vc_column_inner'   => 'lb_column_inner'
         );
-    
+
     //Loop over each element in array
     foreach ($elements as $oldEl => $newClass) {
         //Get current values stored in the class param in element
---
+++ js/lb_custom.js
@@ -0,0 +1,22 @@
+jQuery(function(){ //Run when DOM is loaded
+var $ = jQuery;
+
+$(function langSwitcher() {
+    var raw = $('body').attr('class'),
+        regex = /.+(?:postid-|page-id-)(\d{1,}).+/g,
+        pageId = raw.replace(regex, "$1"),
+        path = window.location.protocol + "//" + window.location.host + "/",
+        lang = $('html').attr('lang'),
+        arr = { "pt": "pt", "es": "es", "it-IT": "it", "fr": "fr", "en-US": "en" }
+
+    console.log("RegEx Page ID: " + pageId);
+
+    $('header .container_inner').append('<div class="lang_switcher"></div>');
+
+    $.each(arr, function( index, value ) {
+        $('.lang_switcher').prepend('<a href="' + path + value + '/index.php?p=' + pageId + '">' + value + '</a>');
+    });
+
+});
+
+});
