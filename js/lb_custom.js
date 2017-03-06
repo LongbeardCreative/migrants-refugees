934ad2f73441ca1871bcff129ec7777f6fb48d09
Evan Hennessy <hello@hennessyevan.com>
2017-03-06 17:46:05 -0500

    Final Round 1 Changes
    Changes to:
    alignment
    accessibility,
    hero image for the stories page
    Improvements for the menu automation
    Lorem text to real copy
    various button and color changes
    
    Added:
    2 Youtube videos
    
    Removed:
    Old github page backups

--- js/lb_custom.js
+++ js/lb_custom.js
@@ -1,11 +1,11 @@
-jQuery(function(){ //Run when DOM is loaded
-var $ = jQuery;
+jQuery(function customJS(){ //Run when DOM is loaded
+var $ = jQuery,
+    raw = $('body').attr('class'),
+    regex = /.+(?:postid-|page-id-)(\d{1,}).+/g,
+    pageId = raw.replace(regex, "$1");
 
 $(function langSwitcher() {
-    var raw = $('body').attr('class'),
-        regex = /.+(?:postid-|page-id-)(\d{1,}).+/g,
-        pageId = raw.replace(regex, "$1"),
-        path = window.location.protocol + "//" + window.location.host + "/",
+    var path = window.location.protocol + "//" + window.location.host + "/",
         lang = $('html').attr('lang'),
         arr = { "pt": "pt", "es": "es", "it-IT": "it", "fr": "fr", "en-US": "en" }
 
@@ -21,6 +21,21 @@ $(function langSwitcher() {
         }
     });
 
+});
+
+// Wrap Stories Articles in HREFs
+$('.boxes_image').each(function(){
+	var href = $('a', this).attr('href');
+
+	$(this).closest('li').children('.latest_post').wrap('<a href="' + href + '"></a>');
+});
+
+$('#popup_menu').click(function(){
+    if (pageId == 9) {
+        $('.menu-main li:first-child').hide();
+    }else if (pageId == 21) {
+        $('.menu-main li:nth-child(2)').hide();
+    }
 });
 
 });
